e5047fedea00b519e03136312022eac3109dd5e3
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2020-12-22 09:45:50 -0500

    !!

--- src/components/Header/PropertyOffer.js
+++ src/components/Header/PropertyOffer.js
@@ -1,22 +1,17 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
-import './Welcome.css'
-import About from './About'
+import './PropertyOffer.css'
 
-const Welcome = ({element}) => {
+const Offer = ({element}) => {
     return (
-            <main>
                 <div className='container-fluid welcome'>
                     <div className='container-fluid pictureArea'>
                         <p className='textOverlay'>Home, anywhere and everywhere</p>
                     </div>
                     </div>
-                <About />
-                <About />
-                <About />
-            </main>
+          
     )
 }
 
 
-export default Welcome
+export default Offer
