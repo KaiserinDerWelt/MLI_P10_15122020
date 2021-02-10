a4d37faeccbbe6d2a80eb77bf9bcf440ccacb7ab
KaiserinDerWelt <desjmarianaguilar@gmail.com>
2021-02-10 15:33:20 -0600

    Modified Card Structure

--- kasabnb/public/assets/js/db.js
+++ kasabnb/public/assets/js/db.js
@@ -621,3 +621,4 @@ const db = {
 }
 
 
+var 
--- kasabnb/src/componentes/home/services/Services.js
+++ kasabnb/src/componentes/home/services/Services.js
@@ -19,121 +19,122 @@ class Servicios extends React.Component {
 				          <div className="card mb-4 shadow-sm">
 							  {/*Modify card background*/}
 							  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
-						  <div className="bottonLeftTittle">Sample Test Takes Place Here</div>
+							  <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 				        <div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
-						  <div className="bottonLeftTittle">Sample</div>
+				          <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 				        <div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
-						  <div className="bottonLeftTittle">Sample</div>
+				          <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 				        <div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
 						  <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 				        <div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
-						  <div className="bottonLeftTittle">Sample</div>
+				          <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 				        <div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
-						  <div className="bottonLeftTittle">Sample</div>
+				          <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 				        <div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
-						  <div className="bottonLeftTittle">Sample</div>
+				          <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 				        <div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
-						  <div className="bottonLeftTittle">Sample</div>
+				          <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 				        <div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
-						  <div className="bottonLeftTittle">Sample</div>
+				          <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 						<div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
-						  <div className="bottonLeftTittle">Sample</div>
+				          <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 						<div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
-						  <div className="bottonLeftTittle">Sample</div>
+				          <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 						<div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
-						  <div className="bottonLeftTittle">Sample</div>
+				          <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 						<div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
-						  <div className="bottonLeftTittle">Sample</div>
+				          <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 						<div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
-						  <div className="bottonLeftTittle">Sample</div>
+				          <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 						<div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
 						  <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 						<div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
 						  <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 						<div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
+				          <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 						<div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
-						  <div className="bottonLeftTittle">Sample</div>
+				          <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 						<div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
-						  <div className="bottonLeftTittle">Sample</div>
+				          <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 						<div className="col-md-4">
 				          <div className="card mb-4 shadow-sm">
 						  <NavLink to="/about" className="nav-link-property"> <img className="bd-placeholder-img card-img-top" width="100%" height="225" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-9-1.jpg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Property 1"/></NavLink>
-				          </div>
-						  <div className="bottonLeftTittle">Sample</div>
+				          <div className="bottonLeftTittle">Sample</div>
+						  </div>
 				        </div>
 				      </div>
 				    </div>
